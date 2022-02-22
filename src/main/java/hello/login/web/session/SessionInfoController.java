60c5e0b2e312952cec39e03b6210daddc7bc42a8
vega2k <vega2k@naver.com>
2022-02-23 00:48:21 +0900

    sessioninfo update

--- src/main/java/hello/login/web/session/SessionInfoController.java
+++ src/main/java/hello/login/web/session/SessionInfoController.java
@@ -1,20 +1,20 @@
 package hello.login.web.session;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.Date;
 
 @Slf4j
-@RestController
+@Controller
 public class SessionInfoController {
 
     @GetMapping("/session-info")
     public String sessionInfo(HttpServletRequest request) {
-        HttpSession session = request.getSession(false);
+        HttpSession session = request.getSession();
         if (session == null) {
             return "세션이 없습니다.";
         }
@@ -29,7 +29,7 @@ public class SessionInfoController {
         log.info("lastAccessedTime={}", new Date(session.getLastAccessedTime()));
         log.info("isNew={}", session.isNew());
 
-        return "세션 출력";
+        return "sessionInfo";
 
     }
 }
