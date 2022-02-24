d2e7033dac977b51aed2d823d437d53443df11f7
vega2k <vega2k@naver.com>
2022-02-25 01:20:39 +0900

    webconfig update

--- src/main/java/hello/typeconverter/WebConfig.java
+++ src/main/java/hello/typeconverter/WebConfig.java
@@ -15,12 +15,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
         //주석처리 우선순위
-//        registry.addConverter(new StringToIntegerConverter());
-//        registry.addConverter(new IntegerToStringConverter());
+        registry.addConverter(new StringToIntegerConverter());
+        registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpPortToStringConverter());
 
         //추가
-        registry.addFormatter(new MyNumberFormatter());
+//        registry.addFormatter(new MyNumberFormatter());
     }
 }
